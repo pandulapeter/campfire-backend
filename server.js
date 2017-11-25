f838f49c041cd77e4bb5752af7a6c5f8759c9abc
Péter Pandula <ppeter@halcyonmobile.com>
2017-11-25 13:39:33 +0200

    Add the ability to query for individual songs.

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
+var http = require('http');
+var url = require('url');
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/library', function(request, response) {
 	fs.readFile( __dirname + "/public/songs.json", 'utf8', function (err, data) {
-		console.log("Opening "+__dirname)
+		console.log("Request for the full library.")
 		response.end(data);
+	});
+})
+
+app.get('/song', function(request, response) {
+	var songId = url.parse(request.url, true).query.id
+	fs.readFile( __dirname + "/public/" + songId + ".cmpf", 'utf8', function (err, data) {
+		console.log("Request for song "+ songId)
+		response.end('{ "id": "'+songId+'", "song": "'+data+'" }');
 	});
 })
 
