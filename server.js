aff5648d56fa4632063a85d303c08e6107b81a23
Péter Pandula <peter@halcyonmobile.com>
2018-03-20 15:46:44 +0200

    Add versioning and opened endpoint.

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ var url = require('url');
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/library', function(request, response) {
+app.get('/v1/library', function(request, response) {
 	fs.readFile( __dirname + "/library.json", 'utf8', function (err, data) {
 		console.log("Request for the full library.")
 		response.end(data);
 	});
 })
 
-app.get('/song', function(request, response) {
+app.get('/v1/song', function(request, response) {
 	var songId = url.parse(request.url, true).query.id
 	fs.readFile( __dirname + "/songs/" + songId + ".txt", 'utf8', function (err, data) {
 		console.log("Request for song " + songId + ".")
 		response.end('{ "id": "'+songId+'", "song": "'+data+'" }');
 	});
+})
+
+app.put('/v1/opened', function(request, response) {
+	var songId = url.parse(request.url, true).query.id
+	console.log("Request to increase popularity of song " + songId + ".")
+	response.end('{ "id": "'+songId+'", "song": "'+data+'" }');
 })
 
 app.listen(app.get('port'), function() {
