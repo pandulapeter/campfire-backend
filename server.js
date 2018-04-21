d1d7a28041ef6cbbb040a56407876d851fc0532b
Péter Pandula <peter@halcyonmobile.com>
2018-04-21 18:22:12 +0300

    Added Collections endpoint.

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/v1/library', function(request, response) {
 	});
 })
 
+app.get('/v1/collections', function(request, response) {
+	fs.readFile( __dirname + "/collections.json", 'utf8', function (err, data) {
+		console.log("Request for the list of collections.")
+		response.end(data);
+	});
+})
+
 app.get('/v1/song', function(request, response) {
 	var songId = url.parse(request.url, true).query.id
 	fs.readFile( __dirname + "/songs/" + songId + ".txt", 'utf8', function (err, data) {
@@ -21,9 +28,15 @@ app.get('/v1/song', function(request, response) {
 	});
 })
 
-app.put('/v1/opened', function(request, response) {
+app.put('/v1/songOpened', function(request, response) {
 	var songId = url.parse(request.url, true).query.id
 	console.log("Request to increase popularity of song " + songId + ".")
+	response.end();
+})
+
+app.put('/v1/collectionOpened', function(request, response) {
+	var collectionId = url.parse(request.url, true).query.id
+	console.log("Request to increase popularity of collection " + collectionId + ".")
 	response.end();
 })
 
